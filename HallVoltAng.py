2163cb130f2e2362be35680c95bebb2119d9b9da
AntiEntity314 <entityevi@gmail.com>
2025-09-21 00:06:15 +0530

    Add files via upload

---
+++ HallVoltAng.py
@@ -0,0 +1,13 @@
+def HallVolt():
+    I = float(input("Enter Current flowing through the wire: "))
+    powI = int(input("Enter the power of 10 for current: "))
+    B = float(input("Enter magnetic field value:  "))
+    powB = int(input("Enter the power of 10 for magnetic field strength: "))
+    n = float(input("Enter the charge carrier density: "))
+    pown = int(input("Enter power of 10 for charge carrier density: "))
+    e = 1.6 * pow(10,-19)
+    w = float(input("Enter the width of conductor: "))
+    poww = float(input("Enter power of 10 for thickness: "))
+    H = ((I*pow(10,powI))*(B*pow(10,powB)))/((n*pow(10,pown))*e*w*pow(10,poww))
+    print(f"Hall Voltage = {H} Volt")
+HallVolt()
